1b17a6ef3fcb1b00be3c692e8edcecb125631908
emijrp <emijrp@gmail.com>
2015-11-21 18:55:32 +0100

    +en

---
+++ tracks-en.js
@@ -0,0 +1,5 @@
+var ArrayTracks = [
+["Article: D. B. Cooper", "D. B. Cooper.ogg", "82", 2374], 
+["Article: Mount Pinatubo", "Mount Pinatubo.ogg", "40", 1784], 
+["Article: 1755 Lisbon earthquake", "1755 Lisbon earthquake.ogg", "57", 953], 
+]
