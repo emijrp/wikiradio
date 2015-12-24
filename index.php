f22ae4444a2255ccef334895722434e3f0984424
Wilfredo Rodríguez <wilfredor@gmail.com>
2015-12-24 18:42:45 -0200

    wikimedia style

--- index.php
+++ index.php
@@ -10,58 +10,58 @@
     <div id="wikiradio" >
         <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Wikiradio-logo.svg" width="180">
     </div>
-    
-    <div id="channels">
-        <p><b>Spoken Wikipedia:</b> 
-        <a href="index.php?channel=ar">العربية</a> · 
-        Català · 
-        Deutsch · 
-        <a href="index.php?channel=en">English</a> · 
-        <a href="index.php?channel=es">Español</a> · 
-        Français · 
-        <a href="index.php?channel=gl">Galego</a> · 
-        <a href="index.php?channel=pt">Português</a> · 
-        <a href="index.php?channel=ru">Русский</a> · 
-        All
-        </p>
-        
-        <p><b>Music:</b> 
-        <a href="index.php?channel=classic">Classic</a> · 
-        <a href="index.php?channel=india">India</a> · 
-        <a href="index.php?channel=tango">Tango</a> · 
-        All
-        </p>
-        
-        <p><b>Topics:</b> 
-        <a href="index.php?channel=anthems">Anthems</a> · 
-        <a href="index.php?channel=birds">Birds</a> · 
-        Nature · 
-        All
-        </p>
-    </div>
-    
-    <div id="listening">
-        <div style="float:right">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/3/39/High-contrast-audio-volume-high.svg" width="15" />
-            <input id="volumeslider" type="range" min="0" max="100" value="100" step="1">
+    <div id="content" class="mw-body" role="main">
+        <div id="channels">
+            <p><b>Spoken Wikipedia:</b> 
+            <a href="index.php?channel=ar">العربية</a> · 
+            Català · 
+            Deutsch · 
+            <a href="index.php?channel=en">English</a> · 
+            <a href="index.php?channel=es">Español</a> · 
+            Français · 
+            <a href="index.php?channel=gl">Galego</a> · 
+            <a href="index.php?channel=pt">Português</a> · 
+            <a href="index.php?channel=ru">Русский</a> · 
+            All
+            </p>
+            
+            <p><b>Music:</b> 
+            <a href="index.php?channel=classic">Classic</a> · 
+            <a href="index.php?channel=india">India</a> · 
+            <a href="index.php?channel=tango">Tango</a> · 
+            All
+            </p>
+            
+            <p><b>Topics:</b> 
+            <a href="index.php?channel=anthems">Anthems</a> · 
+            <a href="index.php?channel=birds">Birds</a> · 
+            Nature · 
+            All
+            </p>
         </div>
-        <div style="float:left"><b>You are listening</b></div>
-        <br><br>
         
-         <span id="audioDescription"></span>
-      
-    </div>
-
-    <audio id="audioPlayer">
-    Your browser does not support the HTML5 Audio element.
-    </audio>
-
-    <audio id="audioPlayerAux">
-    Your browser does not support the HTML5 Audio element.
-    </audio>
-
-    <p id="data"></p>
+        <div id="listening">
+            <div style="float:right">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/3/39/High-contrast-audio-volume-high.svg" width="15" />
+                <input id="volumeslider" type="range" min="0" max="100" value="100" step="1">
+            </div>
+            <div style="float:left"><b>You are listening</b></div>
+            <br><br>
+            
+             <span id="audioDescription"></span>
+          
+        </div>
     
+        <audio id="audioPlayer">
+        Your browser does not support the HTML5 Audio element.
+        </audio>
+    
+        <audio id="audioPlayerAux">
+        Your browser does not support the HTML5 Audio element.
+        </audio>
+    
+        <p id="data"></p>
+    </div>
     <div id="author"><a href="https://meta.wikimedia.org/wiki/Wikiradio_(tool)">Project page</a>. <small><a href="https://github.com/emijrp/wikiradio">Coded</a> by <a href="https://github.com/emijrp/wikiradio/graphs/contributors">contributors</a></small></div>
 
 <?php    
