afad8206dec1afa7ea60dd882a0a0057b76ac86d
Wilfredo Rodríguez <wilfredor@gmail.com>
2015-12-26 13:14:49 -0200

    print test

--- playlist.php
+++ playlist.php
@@ -31,7 +31,7 @@ function getFileUrl($filename)
   $url = API_URL.'?action=query&prop=revisions&titles='.$filename.'&prop=imageinfo&iiprop=url&format=json';
   echo $url;
   $file = getWikiPageContent($url);
-  return reset(json_decode($file['query']['pages'],true))['imageinfo'][0]['url'];
+  return $file['query']['pages'];//['imageinfo'][0]['url'];
 }
 
 if (isset($_GET['name']))
