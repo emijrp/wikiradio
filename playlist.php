19cce3c637c3da66ddc0f838f6c3aa7dd0303d1c
Wilfredo Rodríguez <wilfredor@gmail.com>
2015-12-26 17:17:52 -0200

    testing url

--- playlist.php
+++ playlist.php
@@ -26,6 +26,7 @@ function getWikiPageContent($name)
   //API query
   $name = str_replace(' ','_',$name);
   $url = API_URL.'titles='.$name.'&rvprop=content&formatversion=2&format=json';
+  echo $url;
   $file = getAPI($url);
   if (!is_null($file))
     return $file['query']['pages'][0]['revisions'][0]['content'];
