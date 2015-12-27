5ca37961efc00a9837df401b1aaf73bec7a37bf8
Wilfredo Rodríguez <wilfredor@gmail.com>
2015-12-27 00:07:01 -0200

    removing dump

--- playlist.php
+++ playlist.php
@@ -74,7 +74,6 @@ function getFilesFromPage($pageName)
 {
   $url = API_URL.'prop=links&titles='.$pageName.'&plnamespace=6&format=json';
   $file = getAPI($url);
-  var_dump($file);
   if (!is_null(reset($file['query']['pages'])['links'][0]))
   {
     return reset($file['query']['pages'])['links'][0];
