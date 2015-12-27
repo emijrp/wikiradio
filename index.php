de23feac8bd93236f8cb1077c1d1a6676cda496e
Wilfredo Rodríguez <wilfredor@gmail.com>
2015-12-27 17:41:45 -0200

    All and adding french channel
    All maybe not sence for wikiradio and adding french channel

--- index.php
+++ index.php
@@ -19,25 +19,25 @@
             Deutsch · 
             <a href="index.php?channel=en">English</a> · 
             <a href="index.php?channel=es">Español</a> · 
-            Français · 
+            <a href="index.php?channel=fr">Français</a> · 
             <a href="index.php?channel=gl">Galego</a> · 
             <a href="index.php?channel=pt">Português</a> · 
             <a href="index.php?channel=ru">Русский</a> · 
-            All
+            
             </p>
             
             <p><b>Music:</b> 
             <a href="index.php?channel=classic">Classic</a> · 
             <a href="index.php?channel=india">India</a> · 
             <a href="index.php?channel=tango">Tango</a> · 
-            All
+            
             </p>
             
             <p><b>Topics:</b> 
             <a href="index.php?channel=anthems">Anthems</a> · 
             <a href="index.php?channel=birds">Birds</a> · 
             Nature · 
-            All
+            
             </p>
         </div>
         
