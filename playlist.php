fe4edeb4b33995da5996935ff9de5969e04a7134
Wilfredo Rodríguez <wilfredor@gmail.com>
2015-12-27 20:53:36 -0200

    another format

--- playlist.php
+++ playlist.php
@@ -86,7 +86,7 @@ function isValidSoundExt($filename)
 {
   //Last 3 chars
   $fileext = strtolower(substr($filename, -3));
-  $validext =  array("wav", "ogg", "flac");
+  $validext =  array("wav", "ogg", "flac","oga");
   return (in_array($fileext, $validext));
 }
 
