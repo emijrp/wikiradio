2782db4bfed10937c810bde4620bee2fcc7805c4
Wilfredo Rodríguez <wilfredor@gmail.com>
2015-12-27 13:18:11 -0200

    channel var

--- wikiradio.js
+++ wikiradio.js
@@ -117,7 +117,7 @@ function getHeight(oDiv) {
     return height;
 }
 
-var getList = ($.urlParam('getList') == null) ? (defaultList) : $.urlParam('getList');
+var getList = ($.urlParam('channel') == null) ? (defaultList) : $.urlParam('channel');
 //var getList = 'test';
 
 volumeslider.addEventListener("change",setvolume,false);
