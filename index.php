8a1cf403cd7830a55da31bee9c61cf697649ef4b
Wilfredo Rodríguez <wilfredor@gmail.com>
2015-12-29 12:39:21 -0200

    now its dinamic

--- index.php
+++ index.php
@@ -14,34 +14,7 @@
     </div>
     <div id="content" class="mw-body" role="main">
         <div id="channels">
-            <p><b>Languages:</b> 
-            <a href="index.php?channel=ar">العربية</a> · 
-            Català · 
-            Deutsch · 
-            <a href="index.php?channel=en">English</a> · 
-            <a href="index.php?channel=es">Español</a> · 
-            <a href="index.php?channel=fr">Français</a> · 
-            <a href="index.php?channel=gl">Galego</a> · 
-            <a href="index.php?channel=pt">Português</a> · 
-            <a href="index.php?channel=ru">Русский</a> · 
-            <a href="index.php?channel=pl">Polskich</a>
-            
-            </p>
-            
-            <p><b>Music:</b> 
-            <a href="index.php?channel=classic">Classic</a> · 
-            <a href="index.php?channel=india">India</a> · 
-            <a href="index.php?channel=tango">Tango</a> · 
-            <a href="index.php?channel=USA">USA</a>
-            
-            </p>
-            
-            <p><b>Topics:</b> 
-            <a href="index.php?channel=anthems">Anthems</a> · 
-            <a href="index.php?channel=birds">Birds</a> · 
-            Nature
-            
-            </p>
+           
         </div>
         
         <div id="listening">
