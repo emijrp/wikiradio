965cb074d540a2b50f727105fc954d35dfbce47b
Wilfredo Rodríguez <wilfredor@gmail.com>
2016-01-21 16:52:32 -0200

    enconding

--- playlist.php
+++ playlist.php
@@ -46,7 +46,6 @@ function getHtmlPageContent($name)
 function getGenericWikiHtmlPageContent($API_URL,$name)
 {
   //API query
-  $name=htmlspecialchars($name);
   $url = $API_URL.'prop=revisions&titles='.$name.'&rvprop=content&rvparse=1&redirects=true&format=json';
   
   $file = getAPI($url);
